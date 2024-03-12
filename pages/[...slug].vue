52143f94f9a9e8961be3037cad5f50bb4aebec99
Ikutetsu <allangreenland@gmail.com>
2024-03-12 13:14:10 -0400

    refined passage page

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -1,15 +1,15 @@
 <template>
   <div class="drawer lg:drawer-open bg-gray-50">
     <div
-      class="flex justify-between items-center px-2 bg-gray-300 text-gray-800 fixed w-full"
+      class="flex justify-between items-center px-2 bg-gray-300 text-gray-800 fixed w-full z-10"
     >
       <div class="flex items-center gap-1 py-4">
-        <NuxtLink to="/"><Icon name="mdi:arrow-left" /></NuxtLink>
+        <NuxtLink to="/"><Icon name="mdi:arrow-left" size="24px" /></NuxtLink>
         <h1 class="text-xl font-bold">{{ intro?.title }}</h1>
       </div>
 
       <div class="flex items-center gap-1">
-        <Icon name="mdi:clock" />
+        <Icon name="mdi:clock" size="24px" />
         <span>{{ remainingTime }} mins remaining</span>
       </div>
     </div>
@@ -27,14 +27,14 @@
         class="sticky bottom-8 flex justify-between items-center mx-8 lg:mx-32"
       >
         <NuxtLink
-          class="btn btn-info btn-outline opacity-75"
+          class="btn btn-primary btn-outline opacity-75"
           :class="{ invisible: isBeginning }"
           :to="prev?._path || route.path"
         >
           Back
         </NuxtLink>
         <NuxtLink
-          class="btn btn-info btn-outline opacity-75"
+          class="btn btn-primary btn-outline opacity-75"
           :class="{ invisible: isEnding }"
           :to="next?._path || route.path"
         >
@@ -48,9 +48,18 @@
         aria-label="close sidebar"
         class="drawer-overlay"
       ></label>
-      <ul class="menu px-4 sticky top-16">
-        <li v-for="item in chapters">
-          <NuxtLink :to="item._path">{{ item.title }}</NuxtLink>
+      <ul class="menu steps steps-vertical sticky top-16 ml-4">
+        <li
+          v-for="(item, i) in chapters"
+          class="step"
+          :class="{ 'step-primary ': i + 1 <= currentPos }"
+        >
+          <NuxtLink
+            :to="item._path"
+            :class="{ 'font-semibold': currentPos === i + 1 }"
+          >
+            {{ item.title }}
+          </NuxtLink>
         </li>
       </ul>
     </div>
@@ -101,6 +110,6 @@ onMounted(() => {
 
 <style scoped>
 .nav-btn {
-  @apply btn btn-info btn-outline opacity-75;
+  @apply btn btn-primary btn-outline opacity-75;
 }
 </style>
